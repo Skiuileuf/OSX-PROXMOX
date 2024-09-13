7bcc071a83c58b4394abf12cb3c61cee44f9c3a7
Viziteu Mihai <viziteu.cezar@gmail.com>
2024-09-13 13:39:12 +0300

    Remove random stuff not in scope of this script

--- install.sh
+++ install.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-#########################################################################################################################
-#
-# Script: install
-#
-# https://luchina.com.br
-#
-#########################################################################################################################
+cd /root
 
 clear
 
 if [ -e /root/OSX-PROXMOX ]; then rm -rf /root/OSX-PROXMOX; fi;
-if [ -e /etc/apt/sources.list.d/pve-enterprise.list ]; then rm -rf /etc/apt/sources.list.d/pve-enterprise.list; fi;
-if [ -e /etc/apt/sources.list.d/ceph.list ]; then rm -rf /etc/apt/sources.list.d/ceph.list; fi;
 
 echo "Waiting install OSX-PROXMOX..."
 echo " "
@@ -22,13 +14,7 @@ apt update > /tmp/install-osx-proxmox.log 2>> /tmp/install-osx-proxmox.log
 if [ $? -ne 0 ]
 then 
 	echo " "
-	echo "Error with 'apt-get update' ..."
-	echo "Trying to change /etc/apt/sources.list"
-	echo " "
-
-	sed -i 's/ftp.br.debian.org/ftp.debian.org/g' /etc/apt/sources.list
-		
-	echo "Retrying 'apt-get update' ..."
+	echo "Error with 'apt-get update'. Retrying ..."
 	echo " "
 
 	apt-get update >> /tmp/install-osx-proxmox.log 2>> /tmp/install-osx-proxmox.log
@@ -38,7 +24,7 @@ fi
 
 apt install git -y >> /tmp/install-osx-proxmox.log 2>> /tmp/install-osx-proxmox.log
 
-git clone https://github.com/luchina-gabriel/OSX-PROXMOX.git >> /tmp/install-osx-proxmox.log 2>> /tmp/install-osx-proxmox.log
+git clone https://github.com/Skiuileuf/OSX-PROXMOX.git >> /tmp/install-osx-proxmox.log 2>> /tmp/install-osx-proxmox.log
 
 if [ ! -e /root/OSX-PROXMOX ]; then mkdir -p /root/OSX-PROXMOX; fi;
 
